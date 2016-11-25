56c6b9dcee060650c541300d6dd35cc9f8b525f8
Fernando Giraldo Duque <ferchogir@gmail.com>
2016-11-24 21:10:46 -0500

    Update BD

--- proyecto.sql
+++ proyecto.sql
@@ -1,20 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 3.5.1
+-- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 24-11-2016 a las 21:21:17
--- Versión del servidor: 5.7.14
--- Versión de PHP: 5.6.25
+-- Servidor: localhost
+-- Tiempo de generación: 25-11-2016 a las 02:10:11
+-- Versión del servidor: 5.5.24-log
+-- Versión de PHP: 5.4.3
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `proyecto`
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `drogas`
 --
 
-CREATE TABLE `drogas` (
+CREATE TABLE IF NOT EXISTS `drogas` (
   `Codigo` int(15) NOT NULL,
   `Nombre` varchar(15) NOT NULL,
   `Fabricante` varchar(15) NOT NULL,
@@ -36,7 +36,8 @@ CREATE TABLE `drogas` (
   `Precio` int(20) NOT NULL,
   `Disponibilidad` int(10) NOT NULL,
   `Cod_Proveedor` int(15) NOT NULL,
-  `Formula` varchar(2) DEFAULT NULL
+  `Formula` varchar(2) DEFAULT NULL,
+  PRIMARY KEY (`Codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -44,7 +45,17 @@ CREATE TABLE `drogas` (
 --
 
 INSERT INTO `drogas` (`Codigo`, `Nombre`, `Fabricante`, `Presentacion`, `Tipo`, `Fecha_Vencimiento`, `Precio`, `Disponibilidad`, `Cod_Proveedor`, `Formula`) VALUES
-(1, 'Acetaminofem', 'MK', '100 mg', 'pasta', '2017-11-24', 2000, 100, 4, 'no');
+(1, 'Acetaminofem', 'MK', '100 mg', 'pasta', '2017-11-24', 2000, 100, 4, 'no'),
+(2, 'Aciclovir', 'MK', '800 mg', 'pasta', '2017-12-30', 500, 50, 6, 'no'),
+(3, 'Januvia', 'Jujo Laboratori', '50 mg', 'pasta', '2017-03-30', 1000, 10, 6, 'no'),
+(4, 'Ondax', 'Jujo Laboratori', '8 mg/ml', 'ampolla', '2017-05-25', 5000, 20, 1, 'si'),
+(5, 'Sucralfato', 'Fercho Drogas', '1 g', 'pasta', '2016-12-15', 2500, 30, 10, 'si'),
+(6, 'Asa', 'Jujo Laboratori', '500 mg', 'pasta', '2017-02-25', 10000, 10, 3, 'no'),
+(7, 'Plavix', 'Mk', '75 mg', 'pasta', '2018-06-25', 1500, 35, 9, 'no'),
+(8, 'Naloxona', 'Fercho Drogas', '0.4 mg/ml', 'ampolla', '2017-06-30', 5000, 20, 7, 'si'),
+(9, 'Tramadol', 'Fercho Drogas', '100 mg', 'pasta', '2018-06-05', 1200, 30, 2, 'no'),
+(10, 'Fentanyl', 'Jujo Laboratori', '50 mg/ml', 'ampolla', '2018-09-25', 1350, 25, 10, 'no'),
+(11, 'Gliclazida', 'Mk', '30 mg', 'pasta', '2016-12-21', 500, 47, 6, 'si');
 
 -- --------------------------------------------------------
 
@@ -52,12 +63,13 @@ INSERT INTO `drogas` (`Codigo`, `Nombre`, `Fabricante`, `Presentacion`, `Tipo`, 
 -- Estructura de tabla para la tabla `pedido`
 --
 
-CREATE TABLE `pedido` (
+CREATE TABLE IF NOT EXISTS `pedido` (
   `Cod_Personal` int(15) NOT NULL,
   `Cod_Droga` int(15) NOT NULL,
   `Fecha` date NOT NULL,
   `Cantidad` int(10) NOT NULL,
-  `Precio_Compra` int(15) NOT NULL
+  `Precio_Compra` int(15) NOT NULL,
+  PRIMARY KEY (`Cod_Droga`,`Cod_Personal`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -66,7 +78,7 @@ CREATE TABLE `pedido` (
 -- Estructura de tabla para la tabla `personal`
 --
 
-CREATE TABLE `personal` (
+CREATE TABLE IF NOT EXISTS `personal` (
   `Codigo` int(20) NOT NULL,
   `Contra` int(4) NOT NULL,
   `Primer_Nombre` varchar(15) NOT NULL,
@@ -74,7 +86,8 @@ CREATE TABLE `personal` (
   `Primer_Apellido` varchar(15) NOT NULL,
   `Segundo_Apellido` varchar(15) NOT NULL,
   `Fecha_Nacimiento` date NOT NULL,
-  `Cargo` varchar(20) NOT NULL
+  `Cargo` varchar(20) NOT NULL,
+  PRIMARY KEY (`Codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -100,9 +113,10 @@ INSERT INTO `personal` (`Codigo`, `Contra`, `Primer_Nombre`, `Segundo_Nombre`, `
 -- Estructura de tabla para la tabla `proveedor`
 --
 
-CREATE TABLE `proveedor` (
+CREATE TABLE IF NOT EXISTS `proveedor` (
   `Codigo` int(15) NOT NULL,
-  `Nombre` varchar(20) NOT NULL
+  `Nombre` varchar(20) NOT NULL,
+  PRIMARY KEY (`Codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -119,7 +133,8 @@ INSERT INTO `proveedor` (`Codigo`, `Nombre`) VALUES
 (7, 'DrogasVirginia'),
 (8, 'NeaDrogas'),
 (9, 'PitoDrogas'),
-(10, 'YanembaDrogas');
+(10, 'YanembaDrogas'),
+(11, 'Jujos Laboratorios');
 
 -- --------------------------------------------------------
 
@@ -127,57 +142,15 @@ INSERT INTO `proveedor` (`Codigo`, `Nombre`) VALUES
 -- Estructura de tabla para la tabla `venta`
 --
 
-CREATE TABLE `venta` (
-  `Codigo_Venta` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `venta` (
+  `Codigo_Venta` int(11) NOT NULL AUTO_INCREMENT,
   `Cod_Personal` int(20) NOT NULL,
   `Cod_Droga` int(20) NOT NULL,
   `Cantidad` int(20) NOT NULL,
-  `Total` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Total` int(20) NOT NULL,
+  PRIMARY KEY (`Codigo_Venta`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `drogas`
---
-ALTER TABLE `drogas`
-  ADD PRIMARY KEY (`Codigo`);
-
---
--- Indices de la tabla `pedido`
---
-ALTER TABLE `pedido`
-  ADD PRIMARY KEY (`Cod_Droga`,`Cod_Personal`);
-
---
--- Indices de la tabla `personal`
---
-ALTER TABLE `personal`
-  ADD PRIMARY KEY (`Codigo`);
-
---
--- Indices de la tabla `proveedor`
---
-ALTER TABLE `proveedor`
-  ADD PRIMARY KEY (`Codigo`);
-
---
--- Indices de la tabla `venta`
---
-ALTER TABLE `venta`
-  ADD PRIMARY KEY (`Codigo_Venta`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `venta`
---
-ALTER TABLE `venta`
-  MODIFY `Codigo_Venta` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
